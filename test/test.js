ee0211bb4236db5372e66670f9c043aa61070ebc
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-24 00:32:29 +0200

    Use ava for testing

--- test/test.js
+++ test/test.js
@@ -1,24 +1,22 @@
-/*global describe, it */
 'use strict';
 
-var assert = require('assert');
 var bz2 = require('../');
 var Decompress = require('decompress');
 var fs = require('fs');
 var path = require('path');
+var test = require('ava');
 
-describe('bzip2()', function () {
-    it('should decompress a BZ2 file', function (cb) {
-        var decompress = new Decompress();
+test('should decompress a BZ2 file', function (t) {
+    var decompress = new Decompress()
+        .src(path.join(__dirname, 'fixtures/test.jpg.bz2'))
+        .dest(path.join(__dirname, 'tmp'))
+        .use(bz2());
 
-        decompress
-            .src(path.join(__dirname, 'fixtures/test.jpg.bz2'))
-            .dest(path.join(__dirname, 'tmp'))
-            .use(bz2())
-            .decompress(function (err) {
-                assert(!err);
-                assert(fs.existsSync(path.join(__dirname, 'tmp/test.jpg')));
-                cb();
-            });
+    decompress.decompress(function (err) {
+        t.assert(!err);
+
+        fs.exists(path.join(decompress.dest(), 'test.jpg'), function (exists) {
+            t.assert(exists);
+        });
     });
 });
