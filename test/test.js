3893986544605d67ceee7d77aa805ff186a5ad45
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-29 00:20:07 +0200

    Various improvements

--- test/test.js
+++ test/test.js
@@ -2,11 +2,14 @@
 
 var bz2 = require('../');
 var Decompress = require('decompress');
-var fs = require('fs');
+var exists = require('fs').exists;
 var path = require('path');
+var rm = require('rimraf');
 var test = require('ava');
 
 test('should decompress a BZ2 file', function (t) {
+    t.plan(3);
+
     var decompress = new Decompress()
         .src(path.join(__dirname, 'fixtures/test.jpg.bz2'))
         .dest(path.join(__dirname, 'tmp'))
@@ -15,8 +18,12 @@ test('should decompress a BZ2 file', function (t) {
     decompress.decompress(function (err) {
         t.assert(!err);
 
-        fs.exists(path.join(decompress.dest(), 'test.jpg'), function (exists) {
-            t.assert(exists);
+        exists(path.join(decompress.dest(), 'test.jpg'), function (exist) {
+            t.assert(exist);
+
+            rm(decompress.dest(), function (err) {
+                t.assert(!err);
+            });
         });
     });
 });
