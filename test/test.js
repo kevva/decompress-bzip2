9218849b97223aa25c3469c1cac18b182b379baa
Kevin Martensson <kevinmartensson@gmail.com>
2014-09-13 22:29:59 +0200

    Use vinyl-file for testing

--- test/test.js
+++ test/test.js
@@ -1,22 +1,18 @@
 'use strict';
 
 var bzip2 = require('../');
-var File = require('vinyl');
-var fs = require('fs');
 var isJpg = require('is-jpg');
 var path = require('path');
+var read = require('vinyl-file').read;
 var test = require('ava');
 
 test('decompress a BZ2 file', function (t) {
     t.plan(2);
 
-    fs.readFile(path.join(__dirname, 'fixtures/test.jpg.bz2'), function (err, buf) {
+    read(path.join(__dirname, 'fixtures/test.jpg.bz2'), function (err, file) {
         t.assert(!err);
 
         var stream = bzip2();
-        var file = new File({
-            contents: buf
-        });
 
         stream.on('data', function (file) {
             t.assert(isJpg(file.contents));
